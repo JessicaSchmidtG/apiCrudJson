cc87029be49825d987f0d24ed7cd3372769b0562
Jéssica Schmidt <jessyka_sl@hotmail.com>
2023-07-27 21:44:36 -0300

    add

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+//127.0.0.1 = localhost porta padrao mysql 3306
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+});
+
+export default {
+    connection
+}
--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,87 +1,31 @@
 import {Request, Response} from 'express';
-//Importar o pacote File System para manipular arquivos
-const fs = require('fs');
-//Importar banco de dados de extensão .json
-const data: string = './database.json'; 
+import db from '../config/database';
 
 async function listUsers(req: Request, res: Response) {
-    const jsonData = fs.readFileSync(data);
-    //analisa string JSON
-    res.send(JSON.parse(jsonData));
-}
-
-async function addUser(req: Request, res: Response) {
-    //atribui a base de dados em nova variavel
-    const jsonDataBase = fs.readFileSync(data);
-    
-    //analisa string JSON e transforma em um objeto JavaScript
-    let content = JSON.parse(jsonDataBase);
-    
-    //verifica a quantidade de objetos na base de dados
-    let index: number = Object.keys(content).length;
-    
-    //criar uma nova chave de objeto somando +1 do total de objetos 
-    content[index++] = req.body;
-    
-    //analisa um objeto em JavaScript e transforma em uma string JSON
-    const values = JSON.stringify(content);
-    
-    //lê o arquivo da base de dados e adiciona o novo objeto
-    fs.writeFileSync(data, values);
-    
-    //retorno amigável para o usuário que chamou o endpoint
-    res.status(201).send("User '" + req.body.username + "' registered with successfulll!");
+   db.connection.query('select * from clients', (err, results)=> {
+        console.log(results);
+        res.send(results);
+   });
 };
 
-async function editUser(req: Request, res: Response) {
-     //atribui a base de dados em nova variavel
-     const jsonDataBase = fs.readFileSync(data);
+async function createUser(req: Request, res: Response) {
+    const querySql = 'insert into clients(DS_NAME, NM_CELLPHONE, DS_STATUS) VALUES(?,?,?);';
+    const params = Array(
+        req.body.DS_NAME,
+        req.body.NM_CELLPHONE,
+        req.body.DS_STATUS
+    );
 
-     //recupera o id enviado por parametro
-     const userId = req.params.id;
-     
-     //analisa string JSON e transforma em um objeto JavaScript
-     let content = JSON.parse(jsonDataBase);
-     
-     //atribui os dados da requisição ao usuario existente na base de dados
-     content[userId] = req.body;
-     
-     //analisa um objeto em JavaScript e transforma em uma string JSON
-     const values = JSON.stringify(content);
-     
-     //lê o arquivo da base de dados e adiciona o novo objeto
-     fs.writeFileSync(data, values);
- 
-     //retorno amigável para o usuário que o endpoint
-     res.send(`User with id ${userId} has been updated`)
- };
+    db.connection.query(querySql, params, (err, results) => {
+        res.send('Cadastro realizado com sucesso '+ res.json(results));
+    });
+};
 
- async function deleteUser(req: Request, res: Response) {
-     //atribui a base de dados em nova variavel
-     const jsonDataBase = fs.readFileSync(data);
+//async function editUser(req: Request, res: Response) {};
 
-     //recupera o id enviado por parametro
-     const userId = req.params.id;
-     
-     //analisa string JSON e transforma em um objeto JavaScript
-     let content = JSON.parse(jsonDataBase);
-     
-     //delete
-     delete content[userId];
-     
-     //analisa um objeto em JavaScript e transforma em uma string JSON
-     const values = JSON.stringify(content);
- 
-     //lê o arquivo da base de dados e adiciona o novo objeto
-     fs.writeFileSync(data, values);
- 
-     //retorno amigável para o usuário que o endpoint
-     res.send(`User with id ${userId} has been deleted`);
- };
+//async function deleteUser(req: Request, res: Response) {};
     
 export default {
     listUsers,
-    addUser,
-    editUser,
-    deleteUser
+    createUser
 };
